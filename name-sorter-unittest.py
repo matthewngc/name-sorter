59c5eb1fc2a935beef519acd75a4f0b8235807e5
matthewngc <13530@coderacademy.edu.au>
2024-03-20 18:50:11 +1100

    Added documentation to name_sorter.py & name-sorter-unittest.py

--- name-sorter-unittest.py
+++ name-sorter-unittest.py
@@ -1,22 +1,28 @@
 import unittest
 from name_sorter import Name, NameSorter
 
+# Unit tests for instances of Name class
 class TestName(unittest.TestCase):
+    # Tests that attributes of Name is defined correctly
     def test_valid_name(self):
         name = Name("Matthew Ching Fung Ng")
         self.assertEqual(name.full_name, "Matthew Ching Fung Ng")
         self.assertEqual(name.last_name, "Ng")
         self.assertEqual(name.given_names, ["Matthew", "Ching", "Fung"])
     
+    # Tests that the program throws an exception when the given names are greater than three
     def test_too_many_given_names(self):
         with self.assertRaises(SystemExit):
             Name("Matthew John James Johnson Ng")
 
+    # Tests that the program throws an exception when there are no given names
     def test_no_given_names(self):
         with self.assertRaises(SystemExit):
             Name("Ng")
 
+# Unit test for instances of the NameSorter class
 class TestNameSorter(unittest.TestCase):
+    # Tests that the get_name() method reads the names from the input file correctly
     def test_get_names(self):
         name_sorter = NameSorter("test-names-list.txt")
         name_sorter.get_names()
@@ -25,6 +31,7 @@ class TestNameSorter(unittest.TestCase):
         self.assertEqual(name_sorter.names[1].full_name, "James Jamie Smith")
         self.assertEqual(name_sorter.names[2].full_name, "William Johnson")
     
+    # Tests that the sort_names() method correctly sorts the names alphabetically, first by last name, then by given name(s)
     def test_sort_names(self):
         name_sorter = NameSorter("test-names-list.txt")
         name_sorter.get_names()
--- name_sorter.py
+++ name_sorter.py
@@ -1,12 +1,15 @@
 import sys
 
+# Define class for Names
 class Name:
+    # Initialize Name object with attributes
     def __init__(self, full_name):
         self.full_name = full_name
         self.name_split = full_name.split()
         self.last_name = self.name_split[-1]
         self.given_names = self.name_split[:-1]
 
+        # Validation of name data
         if len(self.given_names) > 3 or len(self.given_names) == 0:
             print(f'Error with name "{self.full_name}"')
             if len(self.given_names) > 3:
@@ -15,14 +18,19 @@ class Name:
                 print("A name must have at least one given name")
             sys.exit(1)
 
+    # Method used to compare two instances of the Name class and determines if one is less than the other
+    # Allows for the use of the sort() function for instances of the Name class
     def __lt__(self, other):
         return (self.last_name, self.given_names) < (other.last_name, other.given_names)
 
+# Defines class for sorting names
 class NameSorter:
+    # Initialize NameSorter object with input file containing list of unsorted names
     def __init__(self, input):
         self.input = input
         self.names = []
     
+    # Read names from input file
     def get_names(self):
         try:
             with open(self.input, 'r') as file:
@@ -31,13 +39,16 @@ class NameSorter:
             print("File could not be found, please check the file name and try again.")
             sys.exit(1)
 
+    # Sort names alphabetically
     def sort_names(self):
         self.names.sort()
 
+    # Print the sorted names to the console
     def print_sorted_names(self):
         for name in self.names:
             print(name.full_name)
 
+    # Write the sorted names to text file
     def write_to_file(self):
         try:
             with open("sorted-names-list.txt", "w") as file:
@@ -48,9 +59,10 @@ class NameSorter:
             print("Unable to write sorted names to file")
             sys.exit(1)
 
+# Define main function to execute sorting and output of sorted names
 def main():
     if len(sys.argv) != 2:
-        print("To run this program, use: python name-sorter.py <input-file-name>")
+        print("To run this program, use: python name_sorter.py <input-file-name>")
         sys.exit(1)
 
     input = sys.argv[1]
