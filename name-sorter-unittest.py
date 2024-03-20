3ff5fa2f7fc96997c3b78441585e18f8dd5ed148
matthewngc <13530@coderacademy.edu.au>
2024-03-20 12:06:48 +1100

    Added unit testing for NameSorter class

--- name-sorter-unittest.py
+++ name-sorter-unittest.py
@@ -15,6 +15,23 @@ class TestName(unittest.TestCase):
     def test_no_given_names(self):
         with self.assertRaises(SystemExit):
             Name("Ng")
-            
+
+class TestNameSorter(unittest.TestCase):
+    def test_get_names(self):
+        name_sorter = NameSorter("test-names-list.txt")
+        name_sorter.get_names()
+        self.assertEqual(len(name_sorter.names), 3)
+        self.assertEqual(name_sorter.names[0].full_name, "James John Smith")
+        self.assertEqual(name_sorter.names[1].full_name, "James Jamie Smith")
+        self.assertEqual(name_sorter.names[2].full_name, "William Johnson")
+    
+    def test_sort_names(self):
+        name_sorter = NameSorter("test-names-list.txt")
+        name_sorter.get_names()
+        name_sorter.sort_names()
+        self.assertEqual(name_sorter.names[0].full_name, "William Johnson")
+        self.assertEqual(name_sorter.names[1].full_name, "James Jamie Smith")
+        self.assertEqual(name_sorter.names[2].full_name, "James John Smith")
+
 if __name__ == "__main__":
     unittest.main()
